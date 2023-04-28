0fa0f36ce69127ebd4f535041d32afa117fc59d9
zebin-dm <zebin.cai@deepmirror.com>
2023-04-28 11:47:55 +0000

    update

--- scripts/build_images_base_gpu.sh
+++ scripts/build_images_base_gpu.sh
@@ -5,7 +5,7 @@ UBUNTU_VERSION="ubuntu20.04"
 MPI_BUILD_ARG="USE_GLOO=1"
 VERSION=$(cat "./VERSION")
 CUDA_VERSION="11.7.1"
-DOCKERHUB_REGISTRY="aliyunregistry.deepmirror.com.cn/dm"
+DOCKERHUB_REGISTRY="zebincai"
 
 docker build -f Dockerfile-base-gpu \
     --build-arg BASE_IMAGE="nvidia/cuda:${CUDA_VERSION}-cudnn8-devel-${UBUNTU_VERSION}" \
@@ -19,10 +19,9 @@ TORCH_VERSION="2.0.0"
 DEEPSPEED_VERSION="0.9.1"
 docker build -f Dockerfile-default-gpu \
     --build-arg BASE_IMAGE=${DOCKERHUB_REGISTRY}/det-environments:py-${PYTHON_VERSION}-cuda-${CUDA_VERSION}-${VERSION} \
-    --build-arg TORCH_PIP="torch==${TORCH_VERSION}" \
+    --build-arg TORCH_PIP="torch==${TORCH_VERSION} torchvision torchaudio" \
     --build-arg TORCH_TB_PROFILER_PIP="torch-tb-profiler" \
     --build-arg TORCH_CUDA_ARCH_LIST="6.0;6.1;6.2;7.0;7.5;8.0" \
-    --build-arg APEX_GIT="https://github.com/determined-ai/apex.git@3caf0f40c92e92b40051d3afff8568a24b8be28d" \
     --build-arg DET_BUILD_NCCL="" \
     --build-arg DEEPSPEED_PIP="deepspeed==${DEEPSPEED_VERSION}" \
     -t ${DOCKERHUB_REGISTRY}/det-dev:torch-${TORCH_VERSION} \
